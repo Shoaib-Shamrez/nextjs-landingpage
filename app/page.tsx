d3b7a619709a87cb78933c35c147a7c7be56a20b
shoaibch05 <shoaibshamrez@gmail.com>
2026-01-31 00:26:12 +0500

    updated Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/sections/Hero";
 import Supporters from "@/components/sections/Supporters";
 import DashboardPreview from "@/components/sections/DashboardPreview";
 import Stats from "@/components/sections/Stats";
-import Features from "@/components/sections/Features";
+
 import Testimonials from "@/components/sections/Testimonials";
 import CTA from "@/components/sections/CTA";
 import FeautredSection from "@/components/sections/feauture";
